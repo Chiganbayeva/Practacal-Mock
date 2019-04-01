e162fdc705d32c47ed35e30a4accd5efaa1e10e8
dc18abi <dc18abi@ENT-D410-02.herts.ac.uk>
2019-04-01 14:21:25 +0100

    Added test cases

--- test/es/uvigo/esei/dojos/swing/todo/core/TodoListTest.java
+++ test/es/uvigo/esei/dojos/swing/todo/core/TodoListTest.java
@@ -44,19 +44,38 @@ public class TodoListTest {
     @Test
 public void testMoveUp() 
 {
-fail("Not yet implemented");
+        System.out.println("Move Up");
+        TodoList instance = new TodoList();
+        instance.add("111");
+        instance.add("www");
+        assertEquals(instance.elementAt(0), "111" );
+        instance.moveUp(1);
+        assertEquals(instance.elementAt(0), "www");
 }
 
 @Test
 public void testMoveDown() 
 {
-fail("Not yet implemented");
+    System.out.println("Move Down");
+        TodoList instance = new TodoList();
+        instance.add("111");
+        instance.add("www");
+        assertEquals(instance.elementAt(0), "111" );
+        instance.moveDown(0);
+        assertEquals(instance.elementAt(0), "www");
+    
 }
 
 @Test
 public void testAdd() 
 {
-fail("Not yet implemented");
+           System.out.println(" Add");
+           TodoList instance = new TodoList();
+           assertEquals( instance.size(),0 );
+           int i  = instance.size();
+           instance.add("111");
+           assertEquals(instance.size(), i);
+            
 }
 
 @Test
@@ -68,13 +87,31 @@ fail("Not yet implemented");
 @Test
 public void testRemoveAt() 
 {
-fail("Not yet implemented");
+        System.out.println(" Remove at");
+        TodoList instance = new TodoList();
+        instance.add("111");
+        instance.add("222");
+        instance.add("333");
+        assertNotEquals( instance.size(), 0 );
+        instance.removeAt(1);
+        assertEquals( instance.elementAt(1), "333" );
+        instance.removeAt(0);
+        assertEquals( instance.elementAt(0), "333" );
 }
 
 @Test
 public void testSize() 
 {
-fail("Not yet implemented");
+        System.out.println(" Test Size");
+        TodoList instance = new TodoList();
+        instance.add("111");
+        instance.add("222");
+        instance.add("333");
+        assertEquals( instance.size(), 3 );
+        instance.removeAt(0);
+        assertEquals(instance.size(), 2 );
+       
+
 }
-    
+
 }
